02c2e28c74ebcb92d55ebace97ef9bf57ed46d85
Rohit Gupta <er.rohitgupta@outlook.com>
2019-05-03 10:39:39 +0530

    updated package name

--- app/src/androidTest/java/com/github/usersearch/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/usersearch/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package com.sample.rohit.githubusersearch
+package com.github.usersearch
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.sample.rohit.githubusersearch", appContext.packageName)
+        assertEquals("com.github.usersearch", appContext.packageName)
     }
 }
